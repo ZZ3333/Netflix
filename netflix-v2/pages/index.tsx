766a2a48a19936a6e60ee17ec6517d636d0707b1
Zahar <zahar.zelenskiy@gmail.com>
2023-04-22 12:46:21 +0100

    clean global.css

--- netflix-v2/pages/index.tsx
+++ netflix-v2/pages/index.tsx
@@ -1,13 +1,8 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <>
+      <h1>Playing with Netflix</h1>
 
-    
     </>
   )
 }
