bcd88791ecfcd41ae0d530be95eea919249b0205
Zahar <zahar.zelenskiy@gmail.com>
2023-05-22 21:22:56 +0100

    create return status 200 on new user

--- netflix-v2/pages/api/register.ts
+++ netflix-v2/pages/api/register.ts
@@ -18,12 +18,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         })
 
         if (existingUser) {
-            return res.status(422).json({error: 'Email Taken'})
+        return res.status(422).json({error: 'Email Taken'})
         }
 
         const hashedPassword = await bcrypt.hash(password, 12)
 
-        const user = await prismadb.user.create();
+        const user = await prismadb.user.create({
+            data: {
+                email,
+                name,
+                hashedPassword,
+                image: '',
+                emailVerified: new Date()
+            }
+        });
+
+        return res.status(200).json(user);
 
     } catch(error) {
         console.log(error);
