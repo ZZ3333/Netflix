0577424025b6e47d73f73ba51616d109d1263ae9
Zahar <zahar.zelenskiy@gmail.com>
2023-05-22 16:09:17 +0100

    create register variable in the auth file

--- netflix-v2/pages/auth.tsx
+++ netflix-v2/pages/auth.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
 
@@ -10,6 +11,20 @@ const Auth = () => {
 
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+    }, []);
+
+    const register = useCallback(async() => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+
+            })
+        } catch(error) {
+            console.log(error)
+        }
+
     }, [])
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg no-repeat bg-center bg-fixed bg-cover]">
