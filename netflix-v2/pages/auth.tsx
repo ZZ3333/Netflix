c2345b8f2e14ca26098111fe7e881c3f2b31ea5a
Zahar <zahar.zelenskiy@gmail.com>
2023-05-25 16:57:31 +0100

    add signin icon buttons fix icons positioning

--- netflix-v2/pages/auth.tsx
+++ netflix-v2/pages/auth.tsx
@@ -5,7 +5,7 @@ import { signIn} from 'next-auth/react';
 import { useRouter } from "next/router";
 
 import { FcGoogle } from 'react-icons/fc';
-import { FaGitHub } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
 
@@ -93,7 +93,38 @@ const Auth = () => {
                         <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover: hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
-                        <div className="flex flex-row items-center gap-4 mt justify-center">
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                            <div className="
+                                w-10
+                                h-10
+                                bg-white
+                                rounded-full
+                                flex
+                                items-center
+                                justify-center
+                                cursor-pointer
+                                hover:opacity-80
+                                transition
+                            ">
+                                <FcGoogle size={30}/>
+                                
+                            </div>
+                            
+                            <div className="
+                                w-10
+                                h-10
+                                bg-white
+                                rounded-full
+                                flex
+                                items-center
+                                justify-center
+                                cursor-pointer
+                                hover:opacity-80
+                                transition
+                            ">
+                                <FaGithub size={30}/>
+                                
+                            </div>
 
                         </div>
                         <p className="text-neutral-500 mt-12">
