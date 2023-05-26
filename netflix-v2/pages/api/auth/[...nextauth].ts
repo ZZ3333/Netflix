99c7516e9efbe25ddc23484a7fa7c0d0ffa75e60
Zahar <zahar.zelenskiy@gmail.com>
2023-05-26 15:06:17 +0100

    add adapter below debug

--- netflix-v2/pages/api/auth/[...nextauth].ts
+++ netflix-v2/pages/api/auth/[...nextauth].ts
@@ -66,6 +66,7 @@ export default NextAuth ({
         signIn: '/auth',
     },
     debug: process.env.NODE_ENV === 'development',
+    adapter: PrismaAdapter(prismadb),
     session: {
         strategy: 'jwt',
     },
