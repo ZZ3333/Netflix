e89bd123d5c5fcac9b0d72f6441df22750438198
Zahar <zahar.zelenskiy@gmail.com>
2023-05-28 18:53:58 +0100

    create async function in the new file current

---
+++ netflix-v2/pages/api/current.ts
@@ -0,0 +1,5 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse);
