088751dd2d2f8b74867af4071434040d8ac10eb5
Zahar <zahar.zelenskiy@gmail.com>
2023-05-28 18:38:05 +0100

    set up conditions to see if sessions exist

--- netflix-v2/lib/serverAuth.ts
+++ netflix-v2/lib/serverAuth.ts
@@ -3,3 +3,10 @@ import { getSession } from "next-auth/react";
 
 import prismadb from '@/lib/prismadb';
 
+const serverAuth = async (req: NextApiRequest) => {
+    const session = await getSession({ req });
+
+    if (!session?.user?.email) {
+        throw new Error('Not signed in')
+    }
+}
