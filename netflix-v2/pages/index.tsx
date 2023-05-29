b7c809389fb39320a00dcac66fe7a106c34801cb
Zahar <zahar.zelenskiy@gmail.com>
2023-05-29 23:54:57 +0100

    add signOut button

--- netflix-v2/pages/index.tsx
+++ netflix-v2/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl text-green-500 text-left">Playing with Netflix</h1>
+      <button onClick={() => signOut()}>Logout!</button>
 
     </>
   )
