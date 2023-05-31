6d9c500ff5c8a2313fe338070bf8bae8b29b7f32
Zahar <zahar.zelenskiy@gmail.com>
2023-05-31 14:56:27 +0100

    import userCurrentUser create user variable in the home function

--- netflix-v2/pages/index.tsx
+++ netflix-v2/pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextPageContext } from "next"
 import { getSession, signOut } from "next-auth/react"
 
+import useCurrentUser from "@/hooks/useCurrentUser";
+
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
@@ -20,9 +22,12 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+
+  const { data: user} = useCurrentUser();
   return (
     <>
       <h1 className="text-2xl text-green-500 text-left">Playing with Netflix</h1>
+      <p className="text-white">Logged in as: {user?.name}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
 
     </>
