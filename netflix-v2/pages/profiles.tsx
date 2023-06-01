15db6c73881b79f2d22bc0defcccb22ef190779f
Zahar <zahar.zelenskiy@gmail.com>
2023-06-01 13:33:41 +0100

    add async func for next page context and await for getSession

--- netflix-v2/pages/profiles.tsx
+++ netflix-v2/pages/profiles.tsx
@@ -1,6 +1,23 @@
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permament: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
+}
+
 
 const Profiles = () => {
     return (
