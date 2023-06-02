68e03012177c74fb6f2dda9c49c76bd8c87cde9e
Zahar <zahar.zelenskiy@gmail.com>
2023-06-02 16:23:52 +0100

    change callbackUrl for git and google signins to profiles

--- netflix-v2/pages/auth.tsx
+++ netflix-v2/pages/auth.tsx
@@ -90,7 +90,7 @@ const Auth = () => {
                         </button>
                         <div className="flex flex-row items-center gap-4 mt-8 justify-center">
                             <div 
-                            onClick={() => signIn('google', { callbackUrl: '/'})}
+                            onClick={() => signIn('google', { callbackUrl: '/profiles'})}
                             className="
                                 w-10
                                 h-10
@@ -108,7 +108,7 @@ const Auth = () => {
                             </div>
                             
                             <div 
-                            onClick={() => signIn('github', { callbackUrl: '/'})}
+                            onClick={() => signIn('github', { callbackUrl: '/profiles'})}
                             className="
                                 w-10
                                 h-10
