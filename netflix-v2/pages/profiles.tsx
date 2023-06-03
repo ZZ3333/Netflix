0d477aa711b784a14e4ae96a2326ccd0ede53404
Zahar <zahar.zelenskiy@gmail.com>
2023-06-03 15:52:14 +0100

    add a new container for styling

--- netflix-v2/pages/profiles.tsx
+++ netflix-v2/pages/profiles.tsx
@@ -24,6 +24,9 @@ const Profiles = () => {
         <div className=" flex items-center h-full justify-center">
             <div className=" flex flex-col">
                 <h1 className="text-3xl md:text-5xl text-white text-center">Who is watching?</h1>
+                <div className="flex items-center justify-center gap-8 mt-10">
+
+                </div>
             </div>
         </div>
     )
