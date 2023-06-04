46ae7897b81bb68dd75bab232b1a7951c9b5ea7b
Zahar <zahar.zelenskiy@gmail.com>
2023-06-04 23:23:20 +0100

    remove return statements from the index file

--- netflix-v2/pages/index.tsx
+++ netflix-v2/pages/index.tsx
@@ -26,9 +26,7 @@ export default function Home() {
   const { data: user} = useCurrentUser();
   return (
     <>
-      <h1 className="text-2xl text-green-500 text-left">Playing with Netflix</h1>
-      <p className="text-white">Logged in as: {user?.email}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
+      
 
     </>
   )
