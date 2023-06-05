3c62b540e9f9d345cddf6908efe4492c333f152f
Zahar <zahar.zelenskiy@gmail.com>
2023-06-05 16:35:24 +0100

    add styling to the Navbar

--- netflix-v2/components/Navbar.tsx
+++ netflix-v2/components/Navbar.tsx
@@ -1,8 +1,21 @@
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="w-full fixed z-40">
+            <div className="
+            px-4
+            md:px-16
+            py-6
+            flex
+            flex-row
+            items-center
+            transition
+            duration-500
+            bg-zinc-900
+            bg-opacity-90
+            ">
+
+            </div>
+        </nav>
     )
 }
 
