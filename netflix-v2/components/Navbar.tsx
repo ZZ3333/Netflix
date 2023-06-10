c2823ec6877ddfc16570cd6ecc9b30da82d62a90
Zahar <zahar.zelenskiy@gmail.com>
2023-06-10 23:34:46 +0100

    add profile icon

--- netflix-v2/components/Navbar.tsx
+++ netflix-v2/components/Navbar.tsx
@@ -53,6 +53,13 @@ const Navbar = () => {
                     <div className="text-gray-200 hover:text-grey-300 cursor-pointer transition">
                         <BsBell />
                     </div>
+                    <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+                        <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+                            <img src="/images/default-blue.png"></img>
+
+                        </div>
+
+                    </div>
 
                 </div>
 
