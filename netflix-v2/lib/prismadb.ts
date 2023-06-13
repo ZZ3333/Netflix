99c12c67744d9fe02678cb5515f4ca994bbd682c
Zahar <zahar.zelenskiy@gmail.com>
2023-06-13 23:44:49 +0100

    fix render error

--- netflix-v2/lib/prismadb.ts
+++ netflix-v2/lib/prismadb.ts
@@ -4,4 +4,4 @@ import { PrismaClient } from "@prisma/client";
 const client = (global as any).prismadb || new PrismaClient();
 if (process.env.NODE_ENV === 'production') (global as any).prismadb = client;
 
-export default client
+export default client;
