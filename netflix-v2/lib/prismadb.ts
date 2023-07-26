f2ecc770d52e012ec6d1276ed6e2f0368bc07e0c
Zahar <zahar.zelenskiy@gmail.com>
2023-07-26 17:58:58 +0100

    fix errors in the prismadb files

--- netflix-v2/lib/prismadb.ts
+++ netflix-v2/lib/prismadb.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 
-const client = (global as any).prismadb || new PrismaClient();
-if (process.env.NODE_ENV === 'production') (global as any).prismadb = client;
+const client = global.prismadb || new PrismaClient();
+if (process.env.NODE_ENV === 'production') global.prismadb = client;
 
 export default client;  
