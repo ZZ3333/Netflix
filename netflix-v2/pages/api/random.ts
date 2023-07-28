1eeb78c3eb02a736674e415af6929ee57cf41b94
Zahar <zahar.zelenskiy@gmail.com>
2023-07-28 18:59:07 +0100

    add find random movies

--- netflix-v2/pages/api/random.ts
+++ netflix-v2/pages/api/random.ts
@@ -14,6 +14,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const movieCount = await prismadb.movie.count()
         const randomIndex = Math.floor(Math.random() * movieCount)
 
+        const randomMovies = await prismadb.movie.findMany({
+            take: 1,
+            skip: randomIndex
+        })
+
     } catch(error) {
         console.log(error);
         return res.status(400).end();
