e79cbadc5608c0d2b5f2c02e5aae246b02136d23
Zahar <zahar.zelenskiy@gmail.com>
2023-07-28 23:05:17 +0100

    add thumbnail

--- netflix-v2/components/Billboard.tsx
+++ netflix-v2/components/Billboard.tsx
@@ -5,7 +5,7 @@ const Billboard =() => {
     const {data} = useBillboard()
     return (
         <div className="relative h-[56.25vw]">
-            <video src={data?.videoUrl}></video>
+            <video poster={data?.thumbnailUrl} src={data?.videoUrl}></video>
         </div>
     )
 }
