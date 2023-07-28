5ca14bdd5cbd9cd945cf764c11a9a60269311db9
Zahar <zahar.zelenskiy@gmail.com>
2023-07-28 23:04:02 +0100

    add video data

--- netflix-v2/components/Billboard.tsx
+++ netflix-v2/components/Billboard.tsx
@@ -5,7 +5,7 @@ const Billboard =() => {
     const {data} = useBillboard()
     return (
         <div className="relative h-[56.25vw]">
-
+            <video src={data?.videoUrl}></video>
         </div>
     )
 }
