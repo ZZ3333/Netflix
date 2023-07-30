4f7b0ea3c976c4cafec860a1eeceffb107999d49
Zahar <zahar.zelenskiy@gmail.com>
2023-07-30 23:21:01 +0100

    create fetcher function

--- netflix-v2/pages/api/movies/index.ts
+++ netflix-v2/pages/api/movies/index.ts
@@ -2,3 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prismadb from '@/lib/prismadb';
 
 import serverAuth from "@/lib/serverAuth";
+import fetcher from "@/lib/fetcher";
+
+export default async function fetcher(req: NextApiRequest, res: NextApiResponse) {
+
+}
