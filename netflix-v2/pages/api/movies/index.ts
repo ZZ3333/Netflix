dbd15aa52bec29a585b88c002c7300cc7fcfdaee
Zahar <zahar.zelenskiy@gmail.com>
2023-07-30 23:26:13 +0100

    finish movies api

--- netflix-v2/pages/api/movies/index.ts
+++ netflix-v2/pages/api/movies/index.ts
@@ -10,6 +10,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     try {
+        await serverAuth(req)
+
+        const movies = await prismadb.movie.findMany();
+        return res.status(200).json(movies);
 
     } catch(error) {
         console.log(error)
