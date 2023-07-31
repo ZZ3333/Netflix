6f92eac36554dcc455421cd55e4080190bba510b
Zahar <zahar.zelenskiy@gmail.com>
2023-07-31 23:00:21 +0100

    install lodash dependencies

---
+++ netflix-v2/components/MovieList.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import {isEmpty} from 'lodash'
+
+const MovieList = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MovieList
--- netflix-v2/pages/index.tsx
+++ netflix-v2/pages/index.tsx
@@ -4,6 +4,7 @@ import { getSession } from "next-auth/react"
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 
 
 export async function getServerSideProps(context: NextPageContext) {
