d883802c69073f8a15cc7825fd4b05ed14425a08
Zahar <zahar.zelenskiy@gmail.com>
2023-07-31 23:09:44 +0100

    create and style Movie List component

--- netflix-v2/components/MovieList.tsx
+++ netflix-v2/components/MovieList.tsx
@@ -1,9 +1,27 @@
 import React from "react"
 import {isEmpty} from 'lodash'
 
-const MovieList = () => {
+interface MovieListProps {
+    data: Record<string, any>[];
+    title: string;
+}
+
+const MovieList: React.FC<MovieListProps> = ({data, title}) => {
+    if (isEmpty(data)) {
+        reurn null
+    }
     return (
-        <div>
+        <div className=" px-4 md:px-12 mt-4 space-y-8">
+            <div>
+                <p className="text-white text-md md:text-xl lg:text-2xl front-semibold md-4">
+                    {title}
+                </p>
+                <div className="grid grid-cols-4 gap-2">
+                    {data.map((movie) => ( 
+                    <div key={movie.id}>movie</div>
+                    ))}
+                </div>
+            </div>
 
         </div>
     )
